359993806bd3119c7ad298f4582a4cb01034fbdd
Vkram <61977791+Vkrvm@users.noreply.github.com>
2024-08-29 15:01:13 +0300

    Enhance: performance

--- app.js
+++ app.js
@@ -56,4 +56,5 @@ app.post('/send-email', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on http://localhost:${PORT}`));
+
